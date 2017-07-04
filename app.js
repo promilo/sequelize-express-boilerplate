06ae5cf413628605fa95fc225f5e18138d5b09ce
Milind Shah <personalmilind@gmail.com>
2017-07-04 13:56:16 -0400

    Added defualt route

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var app = express();
-
 var volleyball = require('volleyball')
-app.use(volleyball)
+var bodyParser = require('body-parser');
+var puppies = require('./puppies');
 
-var server = app.listen(3000, function () {
+
+app.use(volleyball)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/puppies', function(req, res, next) {
+  res.send(puppies);
+})
+
+app.post('/puppies', function(req, res, next) {
+  var puppy = req.body;
+  console.log("puppy req.body", puppy);
+  puppies.push(puppy);
+  console.log(puppies)
+  res.send(puppy)
+})
+
+app.use('*', function(req, res, next) {
+  res.send('this is the default route so you missed all of the other routes')
+})
+
+var server = app.listen(4000, function () {
     console.log('listening on port', server.address().port)
 })
---
+++ puppies.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'arjun',
+    age: 3,
+    favFood: 'bones'
+  },
+  {
+    id: 2,
+    name: 'ray',
+    age: 1,
+    favFood: 'icecream'
+  },
+  {
+    id: 3,
+    name: 'dong',
+    age: 2,
+    favFood: 'laptop'
+  }
+
+]
